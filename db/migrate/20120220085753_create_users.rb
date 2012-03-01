736802309645ec88321925703eba68514ffaba39
David Celis <david@davidcelis.com>
2012-03-01 13:10:25 -0800

    Barebones auth for now.

--- db/migrate/20120220085753_create_users.rb
+++ db/migrate/20120220085753_create_users.rb
@@ -4,11 +4,6 @@ class CreateUsers < ActiveRecord::Migration
       t.string :email,                    :null => false
       t.string :username,                 :null => false
       t.string :password_digest,          :null => false
-      t.string :auth_token
-      t.string :facebook_token
-      t.string :twitter_token
-      t.string :password_reset_token
-      t.datetime :password_reset_sent_at
       t.string :city
       t.string :region
       t.float :lat
