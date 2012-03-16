73c4811f361303fd57a0aaf54a0edf69b3ccf3fb
David Celis <david@davidcelis.com>
2012-03-15 17:07:52 -0700

    Finish up user class

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
 
-  validates_exclusion_of :username, :in => %w( admin superuser root goodbrews guest sign_up sign_in sign_out )
+  validates_exclusion_of :username, :in => %w( admin superuser root goodbrews guest )
   validates_format_of :username, :with => /^\w+$/
   validates_uniqueness_of :username, :case_sensitive => false, :message => "has been taken."
   validates_length_of :username, :within => 4..20
